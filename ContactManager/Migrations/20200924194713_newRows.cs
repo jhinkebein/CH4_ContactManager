4b69a33cb44fc6f3a47f0846e3e96c9b4e644ef6
Jacob Hinkebein <jhinkebein3@my.stlcc.edu>
2020-09-24 16:08:56 -0500

    Add project files.

---
+++ ContactManager/Migrations/20200924194713_newRows.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactManager.Migrations
+{
+    public partial class newRows : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData( 
+               table: "Categories",
+               columns: new[] { "CategoryId", "Name" },
+               values: new object[,]
+               {
+                    { 5, "Bob" } //He's so cool that he belongs in his own category
+               });
+
+            migrationBuilder.InsertData( 
+               table: "Contacts",
+               columns: new[] { "ContactId", "CategoryId", "DateAdded", "Email", "Firstname", "Lastname", "Organization", "Phone" },
+               values: new object[] { 4, 5, new DateTime(2020, 9, 17, 19, 7, 3, 762, DateTimeKind.Local).AddTicks(2507), "bbilliamson@website.com", "Bob", "Billiamson", null, "555-555-4968" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData( //Have to cut bob the person off first or PMC gets angry
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: "4");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "5");
+        }
+    }
+}
---
+++ ContactManager/Migrations/20200924202558_Address.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactManager.Migrations
+{
+    public partial class Address : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>( //Address column in contacts
+               name: "AddressId",
+               table: "Contacts",
+               nullable: false,
+               defaultValue: 1); //Mary runs a cult so she'll appreciate this
+
+            migrationBuilder.CreateTable( //new Address table
+                name: "Addresses",
+                columns: table => new
+                {
+                    AddressId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.AddressId);
+                });
+
+            migrationBuilder.InsertData( //Populate addresses
+                table: "Addresses",
+                columns: new[] { "AddressId", "Address" },
+                values: new object[,]
+                {
+                    { 1, "1 Walton Way" },
+                    { 2, "2 Del Rio Drive" },
+                    { 3, "3 Herrera Hallway" },
+                    { 4, "Billiamson Court" } //They all live in the same city as the user of the list so he didnt feel like putting the city in
+                });
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 1,
+                column: "AddressId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 2,
+                column: "AddressId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 3,
+                column: "AddressId",
+                value: 3);
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 4,
+                column: "AddressId",
+                value: 4);
+
+            migrationBuilder.AddForeignKey( //Create FK in contacts
+                        name: "FK_Contacts_Addresses_AddressId",
+                        table: "Contacts",
+                        column: "AddressId",
+                        principalTable: "Addresses",
+                        principalColumn: "AddressId",
+                        onDelete: ReferentialAction.Cascade);
+        }
+        
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Addresses_AddressId",
+                table: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "Contacts");
+        }
+    }
+}
